05802cfc36f690db58bb4957db977c34aaab521c
AlbertPec <szyba363@gmail.com>
2025-04-28 10:27:59 +0200

    chromosome refactoring, crossing less random, added ranking selection

--- genetic_algorithm.py
+++ genetic_algorithm.py
@@ -34,6 +34,25 @@ class GeneticAlgorithm:
                     break
         return selected_pop
 
+    @staticmethod
+    def ranking_selection(population, selected_no=2):
+        sorted_population = sorted(population, key=lambda x: x.fitness())
+
+        ranks = list(range(1, len(sorted_population) + 1))
+
+        total_rank = sum(ranks)
+
+        selected_pop = []
+        for _ in range(selected_no):
+            pick = random.uniform(0, total_rank)
+            current = 0
+            for chromosome, rank in zip(sorted_population, ranks):
+                current += rank
+                if current >= pick:
+                    selected_pop.append(chromosome)
+                    break
+        return selected_pop
+
     def copy(self):
         new_genes = [copy.deepcopy(gene) for gene in self.genes]
         return Chromosome(new_genes, self.tables, self.groups)
@@ -43,64 +62,29 @@ class GeneticAlgorithm:
         new_population = selected.copy()
         while len(new_population) < len(old_population):
             new_population.extend(self.reproduce(selected))
-        return new_population[:100]
-
-    def clip(self, parents, gene):
-        for parent in parents:
-            for g in parent:
-                if g.table_id == gene.table_id or g.group_id == gene.group_id:
-                    parent.remove(g)
-
-        return parents
+        return new_population[:len(old_population)]
 
     def reproduce(self, selected):
-        #parent1, parent2 = random.choice(selected), random.choice(selected)
-        parents = [random.choice(selected).genes for _ in range(self.num_parents)]
- #       child1, child2 = parent1.cross(parent2)
+        parents = [random.choice(selected) for _ in range(self.num_parents)]
         children = []
-        for i in range(self.num_parents):
-            child = []
-            current = copy.deepcopy(parents)
-            j = 0
-            while not all(not parent for parent in current):
-                if current[j % self.num_parents]:
-                    child.append(random.choice(current[j % self.num_parents]))
-                current = self.clip(current, child[-1])
-                j += 1
-            chromosome = Chromosome(child, self.tables, self.groups)
-            chromosome = self.fix_chromosome(chromosome)
-            children.append(chromosome)
 
+        for _ in range(self.num_parents):
+            child_genes = []
+            for i in range(len(parents[0].genes)):
+                chosen_parent = random.choice(parents)
+                chosen_gene = chosen_parent.genes[i]
+                new_gene = Gene(chosen_gene.group, chosen_gene.table_id, i)
+                child_genes.append(new_gene)
 
+            child = Chromosome(child_genes, self.tables, self.groups)
 
-        # if random.random() <= self.mutation_probability:
-        #     child1.mutate()
-        # if random.random() <= self.mutation_probability:
-        #     child2.mutate()
-        # return [child1, child2]
+            if random.random() < self.mutation_probability:
+                child.mutate()
+
+            children.append(child)
 
         return children
 
-    def fix_chromosome(self, chromosome):
-        assigned_groups = {gene.group_id for gene in chromosome.genes}
-        all_groups = {i for i in range(len(self.groups))}
-        missing_groups = all_groups - assigned_groups
-
-        for missing_group_id in missing_groups:
-            if len(chromosome.genes) >= len(self.tables): break
-            table = random.choice([i for i in range(len(self.tables))])
-            new_gene = Gene(self.groups[missing_group_id],table, missing_group_id)
-            chromosome.genes.append(new_gene)
-
-        seen_groups = set()
-        unique_genes = []
-        for gene in chromosome.genes:
-            if gene.group_id not in seen_groups:
-                unique_genes.append(gene)
-                seen_groups.add(gene.group_id)
-        chromosome.genes = unique_genes
-
-        return chromosome
 
 
     def simulate(self, total_generations):
@@ -113,6 +97,9 @@ class GeneticAlgorithm:
         best_found, counter, generation_idx = population[0], 0, 0
         while generation_idx < total_generations and not self.stop(best_found, best_found.fitness(), counter):
             population = self.evolve(population)
+            # if generation_idx % 100 == 0:
+            #     print("nr of pop" , len(population))
+            #     print([mem.fitness() for mem in population])
             leading_member = max(population, key=lambda member: member.fitness())
             if leading_member.fitness() <= best_found.fitness():
                 counter += 1
